6209c6e3d3ca94b58d33cf43cd45cca7cb8805f5
Matthew Bourke <Matt@Matthews-MacBook-Pro.local>
2017-08-20 12:05:54 +1000

    App now records and plays back recorded sound.

---
+++ SoundBoard/RecordingViewController.swift
@@ -0,0 +1,119 @@
+//
+//  RecordingViewController.swift
+//  SoundBoard
+//
+//  Created by Matthew Bourke on 20/8/17.
+//  Copyright © 2017 Matthew Bourke. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+
+class RecordingViewController: UIViewController {
+    
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var nameTF: UITextField!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var addButton: UIButton!
+    
+    var audioRecorder : AVAudioRecorder? = nil
+    var audioPlayer : AVAudioPlayer? = nil
+    var audioURL : URL?
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        setupRecorder()
+        
+        playButton.isEnabled = false
+    }
+    
+    func setupRecorder() {
+        do {
+            //Create an audio session
+            let session = AVAudioSession.sharedInstance()
+            try session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+            try session.overrideOutputAudioPort(.speaker)
+            try session.setActive(true)
+            
+            // Create URL for audio file
+            let basePath : String = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+            
+            let pathComponents = [basePath, "audio.m4a"]
+            audioURL = NSURL.fileURL(withPathComponents: pathComponents)
+
+            
+            // Create settings for audio recorder
+            var settings : [String : Any] = [:]
+            settings[AVFormatIDKey] = kAudioFormatMPEG4AAC
+            settings[AVSampleRateKey] = 44100.0
+            settings[AVNumberOfChannelsKey] = 2
+            
+            // Create AudioRecorder object
+            try audioRecorder = AVAudioRecorder(url: audioURL!, settings: settings)
+            audioRecorder?.prepareToRecord()
+        } catch let error as NSError {
+            print(error)
+        }
+    }
+    
+    
+    @IBAction func recordTapped(_ sender: Any) {
+        
+        if audioRecorder!.isRecording {
+            // Stop the recording
+            audioRecorder!.stop()
+            
+            // Change button title to 'record'
+            recordButton.setTitle("Record", for: .normal)
+            
+            // Reenable play button
+            playButton.isEnabled = true
+            
+        } else {
+            // Start the recording
+            audioRecorder!.record()
+            
+            // Change button title to 'stop'
+            recordButton.setTitle("Stop", for: .normal)
+        }
+        
+    }
+    
+    @IBAction func playTapped(_ sender: Any) {
+        do {
+        try audioPlayer = AVAudioPlayer(contentsOf: audioURL!)
+            audioPlayer!.play()
+        } catch {
+            
+        }
+    }
+    
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
